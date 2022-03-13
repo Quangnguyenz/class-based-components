fad6a8344dc6bc875b4a4d81c9886a1dda085cbb
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-03-13 17:16:33 -0600

    importing and exporting modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// name and default import/export
+//one default export per module 
+// think of module as a file
+import React from 'react';
+import { name, ticket, person } from './data'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <p> {name} is here with {ticket} </p>
+      <p>{person.name}</p>
+    </section>)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const name = 'Tom'
+export const ticket = 100;
+
+export const person = {
+    name: 'Tom',
+    ticket: 100
+}
+
+const secretValue = 'secret';
