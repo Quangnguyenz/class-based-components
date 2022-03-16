7df74b0c3394a8de9204f8a42286143b68490b99
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-03-15 19:37:54 -0600

    exporting and importing modules

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 // think of module as a file
 import React from 'react';
 import * as data from './data'
+import Banner from './components/Header/Banner'
 
 const App = () => {
   return (
     <section>
       <p> {data.name} is here with {data.ticket} </p>
       <p>{data.person.name}</p>
+      <Banner />
     </section>)
 }
 
---
+++ src/components/Header/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { name } from '../../data'
+
+function Banner() {
+    return (
+        <div>
+            <h1>A Banner {name}</h1>
+        </div>
+    )
+}
+
+export default Banner;
