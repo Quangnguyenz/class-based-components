60282feda2e24750afc44814d976e1eca8dc2612
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-03-23 19:20:44 -0600

    state and map method

--- src/Booklist.js
+++ src/Booklist.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react'
 import Book from "./Book";
 export default class Booklist extends Component {
-
-    books = [
-        {
-            book: "book 1",
-            author: "john doe"
-        },
-        {
-            book: "book 2",
-            author: "bobby doe"
-        }
-    ];
-
+    state = {
+        books: [
+            {
+                book: "book 1",
+                author: "john doe"
+            },
+            {
+                book: "book 2",
+                author: "bobby doe"
+            }
+        ]
+    }
+    // this.setState({})
     render() {
+        const books = this.state.books.map((item) => item.book);
+
         return (
             <section>
                 <h1>A book list</h1>
-                <Book book={this.books[0]} />
-                <Book book={this.books[1]} />
+                <Book book={this.state.books[0]} />
+                <Book book={this.state.books[1]} />
             </section>
         )
     }
