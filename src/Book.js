0679bb6056dc4e897c09c0c92dadad1bbe0e5b59
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-03-27 12:29:30 -0600

    class based component

--- src/Book.js
+++ src/Book.js
@@ -5,10 +5,11 @@ import React, { Component } from 'react'
 export default class Book extends Component {
     render() {
         // console.log(this.props)
-        const { book, author } = this.props.info;
+        const { img, title, author } = this.props.info;
         return (
             <article>
-                <h3>book : {book}</h3>
+                <img src={img} alt="" />
+                <h3>book : {title}</h3>
                 <h5>author : {author}</h5>
             </article>
         )
--- src/bookData.js
+++ src/bookData.js
@@ -2,7 +2,26 @@ const books = [
     {
         id: 1,
         img: "https://images-na.ssl-images-amazon.com/images/I/A1PVJapERIL._AC_UL254_SR254,254_.jpg",
-
+        title: "A book title",
+        author: "Thomas Yun"
+    },
+    {
+        id: 2,
+        img: "https://images-na.ssl-images-amazon.com/images/I/A1PVJapERIL._AC_UL254_SR254,254_.jpg",
+        title: "A book title",
+        author: "Thomas Yun"
+    },
+    {
+        id: 3,
+        img: "https://images-na.ssl-images-amazon.com/images/I/A1PVJapERIL._AC_UL254_SR254,254_.jpg",
+        title: "A book title",
+        author: "Thomas Yun"
+    },
+    {
+        id: 4,
+        img: "https://images-na.ssl-images-amazon.com/images/I/A1PVJapERIL._AC_UL254_SR254,254_.jpg",
+        title: "A book title",
+        author: "Thomas Yun"
     }
 ]
 
